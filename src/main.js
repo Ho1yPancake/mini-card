a935ea7a55dd0d3b2b11a6f9d6a722b91ea32426
Vlad <buyan4ik09@gmail.com>
2026-09-15 21:03:42 +0700

    Initial commit: Vite mini-card

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+let count = 0;
+const btn = document.getElementById('btn');
+const counter = document.getElementById('counter');
+
+btn.addEventListener('click', () => {
+  count++;
+  counter.textContent = `Нажатий: ${count}`;
+});
